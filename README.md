e19b28c41de9c31fe6e01f0b0c5d11f05cf1fdfd
Sadeeptha <hban0006@student.monash.edu>
2022-06-29 16:58:28 +0800

    Added selenium and base code for webscraping

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Automation Scripts in Python
 
 Followed
 https://www.youtube.com/watch?v=PXMJ6FS7llk
+
+Create a .env file to specify all system/usecase specific inputs
--- web_scraping.py
+++ web_scraping.py
@@ -8,3 +8,15 @@
 # //article/div:- / to select the children from the node set on the left side of this character
 # //h1/text(), //article//text
 # //h1/.., ./*, @- Select attribute
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from decouple import config
+
+website = 'https://www.thesun.co.uk/sport/football/'
+path = config('driver_path', default='')
+
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
+driver.get(website)
+
