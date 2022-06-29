be52a7ca139152f2bfc7a39e85d21434edf689b9
Sadeeptha <hban0006@student.monash.edu>
2022-06-29 18:26:57 +0800

    Extracting news from website

--- web_scraping.py
+++ web_scraping.py
@@ -12,6 +12,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from decouple import config
+import pandas as pd
 
 website = 'https://www.thesun.co.uk/sport/football/'
 path = config('driver_path', default='')
@@ -19,4 +20,26 @@ path = config('driver_path', default='')
 service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service)
 driver.get(website)
+
+# Get first occurence - driver.find_element
+# Can use id, path, xpath for searching method
+
+containers = driver.find_elements(by='xpath', value="//div[@class='teaser__copy-container']")
+
+titles, subtitles, links = [], [], []
+
+for container in containers:
+    title = container.find_element(by='xpath', value="./a/h2").text
+    subtitle = container.find_element(by='xpath', value="./a/p").text
+    link = container.find_element(by='xpath', value="./a").get_attribute("href")
+
+    titles.append(title)
+    subtitles.append(subtitle)
+    links.append(link)
+
+news_items = {'titles':titles, 'subtitles':subtitles, 'links': links}
+df_headlines = pd.DataFrame(news_items)   
+df_headlines.to_csv('headline.csv')
+
+driver.quit()
 
--- whatsapp.py
+++ whatsapp.py
@@ -8,4 +8,4 @@ group_id = config('group_id', default='')
 #pywhatkit.sendwhatmsg("+940718195601", "Hello1", 00, 44, 40)
 
 # Group
-pywhatkit.sendwhatmsg_to_group("iPCbMZPbQHHBKIRE7SMp", "Automated msg", 00, 47, 35)# 
+pywhatkit.sendwhatmsg_to_group(group_id,  "Automated msg", 00, 47, 35)# 
